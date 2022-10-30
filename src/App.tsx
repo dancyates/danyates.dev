b7ca7a9effff72241da69d7585dd783c02caf88a
Dan Yates <dyates750@gmail.com>
2022-10-30 16:48:12 +0000

    Add animation library and add boucne in animation to text on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import useWebAnimations, { bounceInDown } from "@wellyshen/use-web-animations";
+
 const App = () => {
+    const { ref } = useWebAnimations<HTMLParagraphElement>({ ...bounceInDown });
+
     return (
         <div className="app">
             <header className="app-header">
-                <p>
+                <p ref={ref}>
                     <b>danyates.dev</b> is coming soon
                 </p>
             </header>
